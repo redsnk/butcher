b78e976e2a1600426bf03015a28abb212852a61c
Your Name <you@example.com>
2025-12-09 07:56:14 +0100

    butcher.c

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include "global.h"
 #include "src/pe/pe.h"
+#include "src/butcher/butcher.h"
 
 int main (int argc, char **argv) {
 struct _PE *pe;
 
     pe = GetPE("/home/alex/src/butcher/samples/api.node");
     if (pe != NULL) {
-		uint8_t *m = GetMemoryPE(pe,0x180002240,0x400);
-        if (m != NULL) {
-            free(m);
+        char *text = Cut(pe,0x180002240);
+        if (text != NULL) {
+            printf("%s\n",text);
         }
 		FreePE(pe);
 	}
---
+++ src/butcher/butcher.h
@@ -0,0 +1,9 @@
+#ifndef _BUTCHER_H
+#define _BUTCHER_H
+
+#include "../../global.h"
+#include "../pe/pe.h"
+
+char *Cut(struct _PE *pe,uint64_t address);
+
+#endif // _BUTCHER_H
