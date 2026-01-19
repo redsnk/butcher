c57d4994ed3684e21f096aa6e86353165c2e84bc
Your Name <you@example.com>
2026-01-19 07:42:07 +0100

    Elf hdr

--- src/files/elf.cpp
+++ src/files/elf.cpp
@@ -1,13 +1,37 @@
 // https://gist.github.com/x0nu11byt3/bcb35c3de461e5fb66173071a2379779
+// https://web.mit.edu/freebsd/head/sys/sys/elf32.h
+// https://web.mit.edu/freebsd/head/sys/sys/elf64.h
 
 #include "elf.hpp"
 
 struct _ELF *GetELF (char *name) {
+FILE *f;
+struct _ELF *elf;
+int l;
+
+	f = fopen(name,"r");
+	if (f!= NULL) {
+		elf = (struct _ELF *) malloc(sizeof(struct _ELF));
+		if (elf != NULL) {
+			// FILE
+			elf->f = f;
+			// Executable Header
+			l = fread(&elf->Ehdr,1,sizeof(union _Elf_Ehdr),f);
+			if (l == sizeof(union _Elf_Ehdr)) {
+
+            }
+            free (elf);
+        }
+        fclose(f);
+    }
     return (NULL);
 }
 
 void FreeELF (struct _ELF *elf) {
-
+    if (elf != NULL) {
+		fclose(elf->f);
+		free(elf);
+	}
 }
 
 uint8_t *GetMemoryELF (struct _ELF *elf, uint64_t addr, uint64_t size, uint64_t *read) {
--- src/files/elf.hpp
+++ src/files/elf.hpp
@@ -7,9 +7,68 @@
 #include <stdint.h>
 #include <cstring>
 
+typedef uint32_t	Elf32_Addr;
+typedef uint16_t	Elf32_Half;
+typedef uint32_t	Elf32_Off;
+typedef int32_t		Elf32_Sword;
+typedef uint32_t	Elf32_Word;
+typedef uint64_t	Elf32_Lword;
+
+typedef uint64_t	Elf64_Addr;
+typedef uint16_t	Elf64_Half;
+typedef uint64_t	Elf64_Off;
+typedef int32_t		Elf64_Sword;
+typedef int64_t		Elf64_Sxword;
+typedef uint32_t	Elf64_Word;
+typedef uint64_t	Elf64_Lword;
+typedef uint64_t	Elf64_Xword;
+
+#define EI_NIDENT (16)
+
+typedef struct
+{
+  unsigned char	e_ident[EI_NIDENT];	/* Magic number and other info */
+  Elf32_Half	e_type;			/* Object file type */
+  Elf32_Half	e_machine;		/* Architecture */
+  Elf32_Word	e_version;		/* Object file version */
+  Elf32_Addr	e_entry;		/* Entry point virtual address */
+  Elf32_Off	e_phoff;		/* Program header table file offset */
+  Elf32_Off	e_shoff;		/* Section header table file offset */
+  Elf32_Word	e_flags;		/* Processor-specific flags */
+  Elf32_Half	e_ehsize;		/* ELF header size in bytes */
+  Elf32_Half	e_phentsize;		/* Program header table entry size */
+  Elf32_Half	e_phnum;		/* Program header table entry count */
+  Elf32_Half	e_shentsize;		/* Section header table entry size */
+  Elf32_Half	e_shnum;		/* Section header table entry count */
+  Elf32_Half	e_shstrndx;		/* Section header string table index */
+} Elf32_Ehdr;
+
+typedef struct
+{
+  unsigned char	e_ident[EI_NIDENT];	/* Magic number and other info */
+  Elf64_Half	e_type;			/* Object file type */
+  Elf64_Half	e_machine;		/* Architecture */
+  Elf64_Word	e_version;		/* Object file version */
+  Elf64_Addr	e_entry;		/* Entry point virtual address */
+  Elf64_Off	e_phoff;		/* Program header table file offset */
+  Elf64_Off	e_shoff;		/* Section header table file offset */
+  Elf64_Word	e_flags;		/* Processor-specific flags */
+  Elf64_Half	e_ehsize;		/* ELF header size in bytes */
+  Elf64_Half	e_phentsize;		/* Program header table entry size */
+  Elf64_Half	e_phnum;		/* Program header table entry count */
+  Elf64_Half	e_shentsize;		/* Section header table entry size */
+  Elf64_Half	e_shnum;		/* Section header table entry count */
+  Elf64_Half	e_shstrndx;		/* Section header string table index */
+} Elf64_Ehdr;
+
+union _Elf_Ehdr {
+	Elf32_Ehdr Ehdr32;
+	Elf64_Ehdr Ehdr64;
+};
+
 struct _ELF {
 	FILE *f;
-	
+	_Elf_Ehdr Ehdr;
 };
 
 #define MAX_IMPORT_NAME	(256)
