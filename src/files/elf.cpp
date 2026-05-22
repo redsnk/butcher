7affa7884e881baae2e73a38eef0ca4943f01768
Your Name <you@example.com>
2026-05-22 11:24:51 +0200

    elf lib undefined

--- src/files/elf.cpp
+++ src/files/elf.cpp
@@ -198,7 +198,7 @@ uint64_t start,end,index;
         // TODO; Elf32
         index = ((addr-start) / 8)+1;
         //strcpy (in->lib_name,"<none>");
-        *lib = strdup("<none>");
+        *lib = strdup("undefined");
         //strcpy (in->func_name,elf->DynStrTable+elf->DynSymTable[index].st_name);
         *func = strdup(elf->DynStrTable+elf->DynSymTable[index].st_name);
         return (true);
